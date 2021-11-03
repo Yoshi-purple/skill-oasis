b2f5ac3fd47d1d6c2707563430f4e57569b6e970
Yoshi-purple <n7ydahew@gmail.com>
2021-11-03 11:00:55 +0900

    gitignoreを書き替えました。

--- skill-oasis/src/main.js
+++ skill-oasis/src/main.js
@@ -6,6 +6,7 @@ import store from './store';
 import Amplify, * as AmplifyModules from 'aws-amplify';
 import {AmplifyPlugin} from 'aws-amplify-vue';
 import awsconfig from './aws-exports';
+
 Amplify.configure (awsconfig);
 
 Vue.use (AmplifyPlugin, AmplifyModules);
@@ -18,3 +19,21 @@ new Vue ({
 
   render: h => h (App),
 }).$mount ('#app');
+
+// const mysql = require ('mysql');
+// const con = mysql.createConnection ({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 1234,
+// });
+// con.connect (err => {
+//   if (err) throw err;
+
+//   console.log ('接続完了');
+
+//   con.query ('CREATE DATABASE NodeTest', (err, result) => {
+//     if (err) throw err;
+//     console.log ('データベースが作成されました');
+//     console.log (result);
+//   });
+// });
